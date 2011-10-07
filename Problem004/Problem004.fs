dfac77cf6c39ef091ad1e673e055a24592459de7
Gene Belitski <gene.belitski@gmail.com>
2011-10-07 12:10:22 -0400

    Problem004 added

---
+++ Problem004/Problem004.fs
@@ -0,0 +1,20 @@
+﻿module ProjectEuler
+
+// Project Euler Problem 4
+// http://projecteuler.net/problem=4
+
+open System
+
+let allProducts l1 l2 =
+    List.map (fun x -> (List.map (fun y -> x * y) l2)) l1 |> List.concat
+
+let isAnagram (s: string) =
+    new String(s.ToCharArray() |> Array.rev) = s
+
+let isPalindromic n = 
+    isAnagram(string n)
+
+let problem004 () =
+    allProducts [100..999] [100..999] |> Seq.distinct
+    |> solve PassAll isPalindromic Seq.max
+
