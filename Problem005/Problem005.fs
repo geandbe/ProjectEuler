e73cd33a285d0bea19929fa326360bb9c7c8bb36
Gene Belitski <gene.belitski@gmail.com>
2011-10-07 13:54:43 -0400

    Problem005 added

---
+++ Problem005/Problem005.fs
@@ -0,0 +1,16 @@
+﻿// Project Euler Problem 5
+// http://projecteuler.net/problem=5
+module ProjectEuler
+
+// gcd - greatest common divisor; Euclidean algorithm:
+let rec gcd n m =
+    match m with
+    | 0L -> n
+    | _ -> gcd m (n % m)
+
+// lcm - least common multiple
+let lcm n m = n * m / gcd n m
+
+let problem005 () =
+    seq [1L .. 20L]
+    |> solve PassAll PassAll (Seq.reduce lcm)
