1c10bfe3edf4415f413931d45c9ac0b1e8d948f7
Gene Belitski <gene.belitski@gmail.com>
2011-10-10 16:54:11 -0400

    Really changed Problem007 implementation

--- Problem007/Problem007.fs
+++ Problem007/Problem007.fs
@@ -2,7 +2,8 @@
 // http://projecteuler.net/problem=7
 module ProjectEuler
 
-let primes =
+// Descending order of already found primes in the list
+let primesD =
     let rec findNextPrime maybePrime lesserPrimes =
         match List.tryFind(fun x -> maybePrime % x = 0) lesserPrimes with
         | Some _ -> findNextPrime (maybePrime + 2) lesserPrimes
@@ -10,8 +11,21 @@ let primes =
 
     Seq.append {2..3} (Seq.unfold (fun (lastPrime, lessOrEqPrimes) ->
                     let nextPrime = findNextPrime (lastPrime + 2) lessOrEqPrimes
-                    Some (nextPrime, (nextPrime, nextPrime::lessOrEqPrimes))) (3,[3;2]))
+                    Some (nextPrime, (nextPrime, nextPrime::lessOrEqPrimes))) (3,[3]))
+
+// Ascending order of already found primes in the list
+let primesA =
+    let rec findNextPrime maybePrime lesserPrimes =
+        match List.tryFind(fun x -> maybePrime % x = 0) lesserPrimes with
+        | Some _ -> findNextPrime (maybePrime + 2) lesserPrimes
+        | None -> maybePrime
+
+    Seq.append {2..3}
+        (Seq.unfold (fun (lastPrime, lessOrEqPrimes) ->
+            let nextPrime = findNextPrime (lastPrime + 2) lessOrEqPrimes
+            Some (nextPrime, (nextPrime, lessOrEqPrimes @ [nextPrime])))
+                (3,[3]))
 
 let problem007 () =
-    primes
+    primesA
     |> solve PassAll PassAll (Seq.skip 10000 >> Seq.head)
