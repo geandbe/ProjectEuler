c318ce8c6c77995a36e795ee1d77fea6a98816e2
Gene Belitski <gene.belitski@gmail.com>
2011-12-04 17:59:28 -0500

    Problem 57 simplified

--- Problems/Problem057.fs
+++ Problems/Problem057.fs
@@ -4,7 +4,7 @@ module ProjectEuler.Problem057
 
 let numberOfDigits n = n.ToString().ToCharArray().GetLength(0)
     
-let expansions = Seq.unfold (fun (n,d) -> Some((n+2I*d,n+d),(n+2I*d,n+d)))(1I,1I)
+let expansions = Seq.unfold (fun (n,d) -> Some((n,d),(n+2I*d,n+d)))(3I,2I)
 
 let problem057 () =
     expansions
