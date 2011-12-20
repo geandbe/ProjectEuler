b1e8ad041d045e8edebe7c9968abfadbf4590c0e
Gene Belitski <gene.belitski@gmail.com>
2011-12-20 17:07:18 -0500

    Problem 64 refactored

--- Problems/Problem064.fs
+++ Problems/Problem064.fs
@@ -5,16 +5,15 @@ module ProjectEuler.Problem064
 // Recurrrent equation to calculate period of periodic square root:
 // http://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Continued_fraction_expansion
 
-let oddPeriod x =
-    let limit = int (sqrt (float x))
-    if limit * limit  = x then
+let oddPeriod s =
+    let limit = int (sqrt (float s))
+    if limit * limit  = s then
         false
     else
         Seq.initInfinite id
         |> Seq.scan (fun (d, m, _) i ->
-                        let newD = (x - m*m)/d
-                        let newM = ((limit + m)/newD)*newD - m
-                        (newD, newM, i + 1)) (1, limit, 0)
+                        let d = (s - m * m) / d in
+                            (d, ((limit + m) / d) * d - m, i + 1)) (1, limit, 0)
         |> Seq.skip 1 |> Seq.skipWhile (fun (d, _, _) -> d <> 1) |> Seq.head
         |> fun (_, _, p) -> p % 2 = 1
 
