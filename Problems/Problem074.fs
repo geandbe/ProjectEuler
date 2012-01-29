76b943341acadc3d2871faf1f9d4471ff1d61378
Gene Belitski <gene.belitski@gmail.com>
2012-01-29 12:36:46 -0500

    Problem 74 refactored to array-based cache

--- Problems/Problem074.fs
+++ Problems/Problem074.fs
@@ -3,8 +3,6 @@
 
 module ProjectEuler.Problem074
 
-open System.Collections.Generic
-
 let digitFact  = function
     | 0 | 1 -> 1 | 2 -> 2 | 3 -> 6 | 4 -> 24 | 5 -> 120
     | 6 -> 720 | 7 -> 5040 | 8 -> 40320 | 9 -> 362880
@@ -15,24 +13,25 @@ let asSumOfDigitsFact n =
         if x = 0 then None else Some(digitFact (x%10), x/10) ) n
     |> Seq.sum
 
-let cache = new Dictionary<int,int>()
-cache.Add(169,3); cache.Add(363601,3); cache.Add(1454,3)
-cache.Add(871,2); cache.Add(872,2); cache.Add(45361,2); cache.Add(45362,2)
+let cache = Array.zeroCreate ((asSumOfDigitsFact 999999) + 1)
+cache.[169] <- 3; cache.[363601] <- 3; cache.[1454] <- 3
+cache.[871] <- 2; cache.[872] <- 2;
+cache.[45361] <- 2; cache.[45362] <- 2; 
 
 let chainLen n =
     let rec buildChain prev len =
         match asSumOfDigitsFact prev with
         | x when x = prev -> len
-        | _ as x -> match cache.TryGetValue x with
-                        | true, l -> len + l
-                        | false, _ -> buildChain x len + 1
+        | _ as x -> match cache.[x] with
+                        | 0 -> buildChain x len + 1
+                        | _ as l -> len + l
 
-    match cache.TryGetValue n with
-    | true, x -> x
-    | false, _ ->
+    match cache.[n] with
+    | 0 ->
         let len = buildChain n 1
-        cache.Add(n, len)
+        cache.[n] <- len
         len
+    | _ as x -> x
 
 let problem074() =
     [1..999999]
