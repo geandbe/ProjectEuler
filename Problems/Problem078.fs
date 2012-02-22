136d9f41746fed8ef03653b7bf6ca9fd24f3544d
Gene Belitski <gene.belitski@gmail.com>
2012-02-22 13:21:28 -0500

    Problem 78 refactoring

--- Problems/Problem078.fs
+++ Problems/Problem078.fs
@@ -3,23 +3,24 @@
 
 module ProjectEuler.Problem078
 
-// http://mathworld.wolfram.com/PartitionFunctionP.html item (11) - Euler's generating function
+// http://mathworld.wolfram.com/PartitionFunctionP.html
+//    item (11) - Euler's generating function
 let problem078 () =
-    let ``P(n)`` (cache: int []) n  =
+    let ``P(n)`` (cache: int[]) n  =
         let mutable recurrence = true
         let mutable k = 1
         while k <= n && recurrence do
             let idx1 = n - k * (3 * k - 1) / 2
             let idx2 = n - k * (3 * k + 1) / 2
-            if idx1 < 0 && idx2 < 0 then recurrence <- false
-            else
+            if idx1 >= 0 || idx2 >= 0 then
                 let sign = if k%2 = 0 then -1 else 1
                 if idx1 >= 0 then cache.[n] <- cache.[n] + sign*cache.[idx1]
                 if idx2 >= 0 then cache.[n] <- cache.[n] + sign*cache.[idx2]
                 k <- k + 1
+            else recurrence <- false
         cache.[n] <- cache.[n] % 1000000
         cache.[n]
 
     let cache = Array.zeroCreate 100000
     cache.[0] <- 1
-    Seq.initInfinite id |> Seq.find (fun n -> ``P(n)`` cache n = 0)
+    Seq.initInfinite id |> Seq.skip 1 |> Seq.find (fun n -> ``P(n)`` cache n = 0)
