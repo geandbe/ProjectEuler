bfff459b7e889b9b8ca187b61877d045b56b4b85
Gene Belitski <gene.belitski@gmail.com>
2012-02-23 21:11:49 -0500

    Problem 78 substituting while/break with function

--- Problems/Problem078.fs
+++ Problems/Problem078.fs
@@ -7,20 +7,39 @@ module ProjectEuler.Problem078
 //    item (11) - Euler's generating function
 let problem078 () =
     let ``P(n)`` (cache: int[]) n  =
-        let mutable recurrence = true
+        let rec ``while/break`` k =
+            if k <= n then
+                let idx1 = n - k * (3 * k - 1) / 2
+                let idx2 = n - k * (3 * k + 1) / 2
+                if idx1 >= 0 || idx2 >= 0 then
+                    let sign = if k%2 = 0 then -1 else 1
+                    if idx1 >= 0 then cache.[n] <- cache.[n] + sign*cache.[idx1]
+                    if idx2 >= 0 then cache.[n] <- cache.[n] + sign*cache.[idx2]
+                    cache.[n] <- cache.[n] % 1000000
+                    ``while/break`` (k + 1)
+        ``while/break`` 1
+        cache.[n]
+
+    let cache = Array.zeroCreate 100000
+    cache.[0] <- 1
+    Seq.initInfinite id |> Seq.find (fun n -> ``P(n)`` cache n = 0)
+
+let problem078I () =
+    let ``P(n)`` (cache: int[]) n  =
+        let mutable recur = true
         let mutable k = 1
-        while k <= n && recurrence do
+        while k <= n && recur do
             let idx1 = n - k * (3 * k - 1) / 2
             let idx2 = n - k * (3 * k + 1) / 2
             if idx1 >= 0 || idx2 >= 0 then
                 let sign = if k%2 = 0 then -1 else 1
                 if idx1 >= 0 then cache.[n] <- cache.[n] + sign*cache.[idx1]
                 if idx2 >= 0 then cache.[n] <- cache.[n] + sign*cache.[idx2]
+                cache.[n] <- cache.[n] % 1000000
                 k <- k + 1
-            else recurrence <- false
-        cache.[n] <- cache.[n] % 1000000
+            else recur <- false
         cache.[n]
 
     let cache = Array.zeroCreate 100000
     cache.[0] <- 1
-    Seq.initInfinite id |> Seq.skip 1 |> Seq.find (fun n -> ``P(n)`` cache n = 0)
+    Seq.initInfinite id |> Seq.find (fun n -> ``P(n)`` cache n = 0)
