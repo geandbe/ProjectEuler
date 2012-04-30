962416e7ea0ef88d42dd98dd71d6d34872db0ddf
Gene Belitski <gene.belitski@gmail.com>
2012-04-30 00:36:17 -0400

    Problem 82 refactored

--- Problems/Problem082.fs
+++ Problems/Problem082.fs
@@ -3,34 +3,27 @@
 module ProjectEuler.Problem082
 
 open System.IO
-open System
 
-let side = 80
-let matrix: int[,] = Array2D.zeroCreate side side
-let shortest: int[] = Array.zeroCreate side
-
+[<Literal>]
+let SIDE = 80
+let matrix: int[,] = Array2D.zeroCreate SIDE SIDE
+let shortest: int[] = Array.zeroCreate SIDE
 
 let getData (path: string) =
     use sr = new StreamReader(path)
-    let line = ref 0
-    let col = ref 0
+    let line, col = ref 0, ref 0
     while not sr.EndOfStream do
         col := 0
         sr.ReadLine().Split(',') |> Array.iter (fun x -> matrix.[!line, !col] <- int x; incr col)
         incr line
 
-
 let problem082() =
     getData @"..\..\..\Datafiles\Problem081.data" // same data P.81 & 82
-    for i = 0 to side - 1 do
-        shortest.[i] <- matrix.[i, side - 1]
-
-    for i = side - 2 downto 0 do
-        shortest.[0] <- shortest.[0] + matrix.[0, i]
-        for j = 1 to side - 1 do
-            shortest.[j] <- min (shortest.[j - 1] + matrix.[j, i]) (shortest.[j] + matrix.[j, i])
-        for j = side - 2 downto 0 do
-            shortest.[j] <- min shortest.[j] (shortest.[j + 1] + matrix.[j, i])
-
+    List.iter (fun row -> shortest.[row] <- matrix.[row, SIDE-1]) [0..SIDE-1]
+    List.iter (fun col ->
+        shortest.[0] <- shortest.[0] + matrix.[0, col]
+        List.iter (fun i -> shortest.[i] <- matrix.[i, col] + min shortest.[i-1] shortest.[i]) [1..SIDE-1]
+        List.iter (fun i -> shortest.[i] <- min shortest.[i] (shortest.[i+1] + matrix.[i, col])) [SIDE-2..-1..0]
+        ) [SIDE-2..-1..0]
     Array.min shortest
 
---
+++ Solver/Whiteboard.fsx
@@ -0,0 +1,17 @@
+﻿open System
+open System.Linq
+
+[<Literal>]
+let SIZE = 10
+
+let vector : int[] = Array.zeroCreate SIZE
+
+let matrix : int[,] = Array2D.zeroCreate SIZE SIZE
+
+let idxs = Enumerable.Range(0, SIZE).Select(fun i -> vector.[i] <- 1)
+let idxs' = Enumerable.Range(0, SIZE).Select(fun i -> printfn "%d" i)
+
+for i in Enumerable.Range(0, 10) do (fun i -> printfn "%d" i)(i)
+
+[32..-1..0]
+for i in Enumerable.Range(0, 10).OrderByDescending(fun x -> x) do (fun i -> printfn "%d" i)(i)
