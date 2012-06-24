03c6d39d50307cd231bc19921734aa847744164d
Gene Belitski <gene.belitski@gmail.com>
2012-06-24 14:57:12 -0400

    Problem 085 slightly refactored

--- Problems/Problem085.fs
+++ Problems/Problem085.fs
@@ -2,13 +2,14 @@
 // http://projecteuler.net/problem=85
 module ProjectEuler.Problem085
 
-let squares x y = x * (x + 1) * y * (y + 1) / 4
-let solutions = Seq.unfold(fun y ->
-                let x = int(sqrt(float (8000000/(y*(y+1)))))
-                let solution = if squares x y > 2000000 then (x - 1,y) else (x,y)
-                if fst solution <= y then None else Some(solution, y + 1)) 1
+let rectangles w h = w * (w + 1) * h * (h + 1) / 4
+let solutions =
+    Seq.unfold(fun h ->
+        let w = int(sqrt(float (8000000/(h*(h+1)))))
+        let solution = if rectangles w h > 2000000 then (w - 1,h) else (w,h)
+        if fst solution <= h then None else Some(solution, h + 1)) 1
 
 let problem085() =
     solutions |> Seq.toList
-    |> List.map (fun (x,y) -> (2000000 - (squares x y), x*y))
-    |> List.sortBy (fun (x,y) -> x) |> List.head |> snd
+    |> List.map (fun (w,h) -> (2000000 - (rectangles w h), w*h))
+    |> List.sortBy (fun (diff,area) -> diff) |> List.head |> snd
